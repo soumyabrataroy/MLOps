c0c1f66bbd418553a7dc9ff43199df814eefdb02
Soumyabrata Roy <46237589+soumyabrataroy@users.noreply.github.com>
2023-08-01 09:53:21 +0530

    Add files via upload

---
+++ model_as_code/another_python_app.py
@@ -0,0 +1,11 @@
+import house_price_predictor
+
+model_object = house_price_predictor.HousePricePredictor()
+
+model_object.predict_price(2)
+
+
+# import os
+# command = 'python house_price_predictor_2.py --distance 1'
+# os.system(command)
+
